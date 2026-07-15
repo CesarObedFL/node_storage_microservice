833aa0612d305ab5129fb37c0791676c8bf166b7
CesarObedFL <cesarobedfl@gmail.com>
2026-07-15 11:32:35 -0600

    [FIX] ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,22 @@
 module.exports = {
-  apps: [
-    {
-      name: 'node_storage_microservice',
-      script: './server.js',
-      watch: true,
-      env: {
-        NODE_ENV: 'production',
-        PORT: "4000",
-      }
+  apps : [{
+    script: 'server.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
     }
-  ]
+  }
 };
